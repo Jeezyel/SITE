5c37e90bb90e1d57b3ec0a75084debdb52d30194
Jeezyel P Costa <72239179+Jeezyel@users.noreply.github.com>
2024-03-06 09:50:28 -0300

    estrutura de pasta

--- home.js
+++
@@ -1,9 +0,0 @@
-function inscreverse(){
-    window.location.href = "https://www.youtube.com";// redirecionando para a formulario
-}
-function detalhe(){
-    window.location.href = "https://www.youtube.com";// redirecionando para a formulario
-}
-function imagem(){
-    window.location.href= "https://music.youtube.com/watch?v=0VZghMkvN6c&list=RDAMVM0VZghMkvN6c"; // so para gastar mesmos depois isso vai ser apagado 
-}
